b2fed9b7d7419063b66278f10ecd91f7ea177dce
Niklas Sankell <niklas_san@gmail.com>
2021-10-25 00:15:33 +0200

    merged scalable with development. Cleaned some lines.

--- src/com/company/ButtonGenerator.java
+++ src/com/company/ButtonGenerator.java
@@ -2,12 +2,9 @@ package com.company;
 
 import java.awt.*;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import java.util.Arrays;
 
 public class ButtonGenerator {
     MyButton [][] buttonArray;
-    ArrayList<MyButton> buttonList;
 
     public ButtonGenerator(int baseSize, ActionListener a){
         buttonArray = new MyButton[baseSize][baseSize];
@@ -17,15 +14,12 @@ public class ButtonGenerator {
                 buttonArray[i][j]=(new MyButton(counter,a));
                 counter++;
             }
-
         }
         buttonArray[baseSize-1][baseSize-1].setText("");
         buttonArray[baseSize-1][baseSize-1].setBackground(Color.black);
-        //buttonList = new ArrayList(Arrays.asList(buttonArray));
     }
 
     public MyButton[][] getButtons() {
         return buttonArray;
     }
-
 }
