bc8a8a50ceb50b431a2545ae4aa80e51ee8ae5b8
Niklas Sankell <niklas_san@gmail.com>
2021-10-25 09:25:36 +0200

    small update Myframe

--- src/com/company/MyFrame.java
+++ src/com/company/MyFrame.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 public class MyFrame extends JFrame {
         MyPanel myPanel;
 
-    public MyFrame(){
+    public MyFrame() {
         myPanel = new MyPanel();
         add(myPanel);
         setVisible(true);
