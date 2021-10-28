c5b688ad3333cfd301afc88ba6b7188ff8a80073
Daniel <e.danielsvensson@gmail.com>
2021-10-28 14:50:52 +0200

    Added options for 2 x 2, 5 x 8

--- src/com/company/MainFrame.java
+++ src/com/company/MainFrame.java
@@ -18,11 +18,15 @@ public class MainFrame extends JFrame {
 
             remove(gamePanel);
             if(e.getSource() == menuPanel.newGameMenu.getItem(0)) {
-                gamePanel = new GamePanel(3,3, colors.currentColor1, colors.currentColor2);
+                gamePanel = new GamePanel(2,2, colors.currentColor1, colors.currentColor2);
             } else if(e.getSource() == menuPanel.newGameMenu.getItem(1)){
-                gamePanel = new GamePanel(4,4, colors.currentColor1, colors.currentColor2);
+                gamePanel = new GamePanel(3,3, colors.currentColor1, colors.currentColor2);
             } else if(e.getSource() == menuPanel.newGameMenu.getItem(2)) {
+                gamePanel = new GamePanel(4,4, colors.currentColor1, colors.currentColor2);
+            } else if(e.getSource() == menuPanel.newGameMenu.getItem(3)) {
                 gamePanel = new GamePanel(5,5, colors.currentColor1, colors.currentColor2);
+            } else if(e.getSource() == menuPanel.newGameMenu.getItem(4)) {
+                gamePanel = new GamePanel(5,8, colors.currentColor1, colors.currentColor2);
             }
 
             add(gamePanel);
--- src/com/company/MenuItems.java
+++ src/com/company/MenuItems.java
@@ -5,7 +5,7 @@ import java.awt.event.ActionListener;
 
 public class MenuItems {
     JMenuItem color1, color2, color3;
-    JMenuItem newGame1, newGame2, newGame3;
+    JMenuItem newGame1, newGame2, newGame3, newGame4, newGame5;
     JMenuItem[] colorMenuItems;
     JMenuItem[] newGameMenuItems;
 
@@ -17,15 +17,20 @@ public class MenuItems {
       color2.addActionListener(a);
       color3.addActionListener(a);
 
-      newGame1 = new JMenuItem("Size 3 x 3");
-      newGame2 = new JMenuItem("Size 4 x 4");
-      newGame3 = new JMenuItem("Size 5 x 5");
+      newGame1 = new JMenuItem("Size 2 x 2");
+      newGame2 = new JMenuItem("Size 3 x 3");
+      newGame3 = new JMenuItem("Size 4 x 4");
+      newGame4 = new JMenuItem("Size 5 x 5");
+      newGame5 = new JMenuItem("Size 5 x 8");
+
       newGame1.addActionListener(a);
       newGame2.addActionListener(a);
       newGame3.addActionListener(a);
+      newGame4.addActionListener(a);
+      newGame5.addActionListener(a);
 
       colorMenuItems = new JMenuItem[] {color1, color2, color3};
-      newGameMenuItems = new JMenuItem[] {newGame1, newGame2, newGame3};
+      newGameMenuItems = new JMenuItem[] {newGame1, newGame2, newGame3, newGame4, newGame5};
     }
 
     public JMenuItem[] getColorMenuItems() {
