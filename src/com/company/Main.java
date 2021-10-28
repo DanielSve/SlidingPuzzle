15d648e497d94197aa9b3d299013147c57f6dbab
Daniel <e.danielsvensson@gmail.com>
2021-10-28 17:17:09 +0200

    small namechange

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        MyFrame frame = new MyFrame();
+        MainFrame frame = new MainFrame();
     }
 }
