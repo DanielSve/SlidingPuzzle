2a003131c3dde752cc5a1b0758adb2df8cc54ad7
Niklas Sankell <niklas_san@gmail.com>
2021-10-29 00:10:04 +0200

    small fixes

--- src/com/company/MyButton.java
+++ src/com/company/MyButton.java
@@ -7,7 +7,6 @@ import java.awt.event.ActionListener;
 public class MyButton extends JButton {
 
     int nr;
-
     public MyButton(int nr, ActionListener a, Color c1, Color c2) {
         this.nr = nr;
         setBackgroundColor(c1,c2);
@@ -23,11 +22,7 @@ public class MyButton extends JButton {
         return nr;
     }
 
-    public void setNr(int number) {
-        this.nr = number;
-    }
-
-    public void setBackgroundColor(Color c1, Color c2){
+    public void setBackgroundColor(Color c1, Color c2) {
         if (nr % 2 == 0) {
             setBackground(c1);
         } else {
