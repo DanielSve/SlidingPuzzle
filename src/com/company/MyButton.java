b1ff7e3e6415b9fc425a6ed98f713dc0fb591cae
Niklas Sankell <niklas_san@gmail.com>
2021-10-29 07:48:42 +0200

    button colors fix

--- src/com/company/MyButton.java
+++ src/com/company/MyButton.java
@@ -7,8 +7,12 @@ import java.awt.event.ActionListener;
 public class MyButton extends JButton {
 
     int nr;
-    public MyButton(int nr, ActionListener a, Color c1, Color c2) {
+    int row;
+    int columns;
+    public MyButton(int nr, int row, int columns, ActionListener a, Color c1, Color c2) {
         this.nr = nr;
+        this.row = row;
+        this.columns = columns;
         setBackgroundColor(c1,c2);
         setBorder(BorderFactory.createLineBorder(Color.black));
         setOpaque(true);
@@ -23,10 +27,26 @@ public class MyButton extends JButton {
     }
 
     public void setBackgroundColor(Color c1, Color c2) {
-        if (nr % 2 == 0) {
-            setBackground(c1);
+        if (columns % 2 == 0) {
+            if (row % 2 == 0) {
+                if (nr % 2 == 0) {
+                    setBackground(c1);
+                } else {
+                    setBackground(c2);
+                }
+            } else {
+                if (nr % 2 != 0) {
+                    setBackground(c1);
+                } else {
+                    setBackground(c2);
+                }
+            }
         } else {
-            setBackground(c2);
+            if (nr % 2 == 0) {
+                setBackground(c1);
+            } else {
+                setBackground(c2);
+            }
         }
     }
 }
